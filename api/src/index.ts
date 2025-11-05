62f8596829d112bd277a454040787bebe6e9fc15
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-05 18:42:18 +0100

    feat: initialise web(next.js) and api(Express TS)

---
+++ api/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+// import routes from "./routes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur l'API LS-Streaming !");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
