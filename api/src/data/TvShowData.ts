9931c70c67f22ad4cb1a7eaf644f127a37d6b6ad
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-06 19:41:28 +0100

    feat-data: integrate data videos

---
+++ api/src/data/TvShowData.ts
@@ -0,0 +1,200 @@
+export const tvShowData = [
+  {
+    id: "T001",
+    title: "Les Traîtres",
+    genre: "game-show",
+    image: "/images/showsTV/traites.png",
+    director: "Françis Côté",
+    seasonData: [
+      {
+        seasonYear: 2022,
+        seasonNumber: 1,
+        seasonTVHost: "Éric Antoine",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            numberEpisode: 7,
+            duration: 60,
+          },
+        ],
+      },
+      {
+        seasonYear: 2023,
+        seasonNumber: 2,
+        seasonTVHost: "Éric Antoine",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            numberEpisode: 7,
+            duration: 60,
+          },
+          {
+            numberEpisode: 8,
+            duration: 60,
+          },
+          {
+            numberEpisode: 9,
+            duration: 60,
+          },
+          {
+            numberEpisode: 10,
+            duration: 60,
+          },
+        ],
+      },
+      {
+        seasonYear: 2024,
+        seasonNumber: 3,
+        seasonTVHost: "Éric Antoine",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            numberEpisode: 7,
+            duration: 60,
+          },
+          {
+            numberEpisode: 8,
+            duration: 60,
+          },
+          {
+            numberEpisode: 9,
+            duration: 60,
+          },
+          {
+            numberEpisode: 10,
+            duration: 60,
+          },
+          {
+            numberEpisode: 11,
+            duration: 60,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "T002",
+    title: "Fort Boyard",
+    genre: "game-show",
+    image: "/images/showsTV/fort-boyard.png",
+    director: "Françis Côté",
+    seasonData: [
+      {
+        seasonYear: 2022,
+        seasonNumber: 34,
+        seasonTVHost: "Olivier Minne",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 90,
+          },
+          {
+            numberEpisode: 2,
+            duration: 90,
+          },
+          {
+            numberEpisode: 3,
+            duration: 90,
+          },
+          {
+            numberEpisode: 4,
+            duration: 90,
+          },
+          {
+            numberEpisode: 5,
+            duration: 90,
+          },
+          {
+            numberEpisode: 6,
+            duration: 90,
+          },
+          {
+            numberEpisode: 7,
+            duration: 90,
+          },
+          {
+            numberEpisode: 8,
+            duration: 90,
+          },
+          {
+            numberEpisode: 9,
+            duration: 90,
+          },
+          {
+            numberEpisode: 10,
+            duration: 90,
+          },
+        ],
+      },
+    ],
+  },
+];
---
+++ api/src/data/seriesData.ts
@@ -0,0 +1,198 @@
+export const seriesData = [
+  {
+    id: "S001",
+    title: "Columbo",
+    genre: "Policier",
+    image: "/images/series/columbo.png",
+    seasonData: [
+      {
+        seasonYear: 1989,
+        seasonNumber: 9,
+        episodes: [
+          {
+            title: "Meurtre, un autoportrait",
+            numberEpisode: 1,
+            duration: 90,
+            director: "James Frawley",
+          },
+          {
+            title: "Columbo crie au Loup",
+            numberEpisode: 2,
+            duration: 90,
+            director: "William Read Woodfield",
+          },
+          {
+            title: "Votez pour moi",
+            numberEpisode: 3,
+            duration: 90,
+            director: "Patrick McGoohan",
+          },
+        ],
+      },
+      {
+        seasonYear: 1990,
+        seasonNumber: 10,
+        episodes: [
+          {
+            title: "Criminologie appliquée",
+            numberEpisode: 1,
+            duration: 90,
+            director: "E.W. Swackhamer",
+          },
+          {
+            title: "Le meurtre peut nuire à la santé",
+            numberEpisode: 2,
+            duration: 86,
+            director: "Daryl Duke",
+          },
+          {
+            title: "Jeux d'ombres",
+            numberEpisode: 3,
+            duration: 92,
+            director: "Alan J Levi",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "S002",
+    title: "Rome",
+    genre: "Historique",
+    image: "/images/series/rome.png",
+    seasonData: [
+      {
+        seasonYear: 2005,
+        seasonNumber: 1,
+        episodes: [
+          {
+            title: "Le Vol de l'Aigle",
+            numberEpisode: 1,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Une République Fragile",
+            numberEpisode: 2,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Le Venin de Cerbère",
+            numberEpisode: 3,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Bon Augures, Mauvais Présages",
+            numberEpisode: 4,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Jeux De Dupes",
+            numberEpisode: 5,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Octave Devient Un Homme",
+            numberEpisode: 6,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Vaincre Ou Mourir",
+            numberEpisode: 7,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Un trône Pour Deux",
+            numberEpisode: 8,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Marchés De Dupes",
+            numberEpisode: 9,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Le Triomphe de César",
+            numberEpisode: 10,
+            duration: 55,
+            director: "Michael Apted",
+          },
+        ],
+      },
+      {
+        seasonYear: 2007,
+        seasonNumber: 2,
+        episodes: [
+          {
+            title: "L'Héritage de César",
+            numberEpisode: 1,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Fils de Hadès",
+            numberEpisode: 2,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Le message de Cicéron",
+            numberEpisode: 3,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Frères ennemis",
+            numberEpisode: 4,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Octave, jeune consul",
+            numberEpisode: 5,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "La Liste d'Octave",
+            numberEpisode: 6,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Le Mariage de Marc Antoine",
+            numberEpisode: 7,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Secrets et Trahisons",
+            numberEpisode: 8,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Le Voyage à Alexandrie",
+            numberEpisode: 9,
+            duration: 55,
+            director: "Michael Apted",
+          },
+          {
+            title: "Au sujet de ton père",
+            numberEpisode: 10,
+            duration: 55,
+            director: "Michael Apted",
+          },
+        ],
+      },
+    ],
+  },
+];
