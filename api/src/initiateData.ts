4ecce294aa2312b1f790147af4e7f508786e2014
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-11 18:03:57 +0100

    featInitiateData: back initialize and populate documentaries data

--- api/src/initiateData.ts
+++ api/src/initiateData.ts
@@ -1,11 +1,17 @@
 import { MovieLibrary } from "./MovieLibrary";
 import { Film } from "./models/Film";
+import { Documentary } from "./models/Documentary";
 
 import { filmsData } from "./data/filmsData";
+import { documentariesData } from "./data/documentariesData";
 
+// Create a new instance of MovieLibrary
 const movieLibrary = new MovieLibrary();
 
+// Function to initiate data
+// and populate the movie library
 function initiateData() {
+  // Add films to the library
   filmsData.forEach((film) => {
     const newFilm = new Film(
       film.id,
@@ -16,7 +22,21 @@ function initiateData() {
       film.realiseDate,
       film.director
     );
-    movieLibrary["videos"].push(newFilm);
+    movieLibrary.add(newFilm);
+  });
+
+  // Add documentaries to the library
+  documentariesData.forEach((doc) => {
+    const newDocumentary = new Documentary(
+      doc.id,
+      doc.title,
+      doc.genre,
+      doc.image,
+      doc.duration,
+      doc.realiseDate,
+      doc.subject
+    );
+    movieLibrary.add(newDocumentary);
   });
 
   return movieLibrary;
