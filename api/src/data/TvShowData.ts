aac553817e42f296279fcff2a8d777c865c1911c
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-12-15 13:11:59 +0100

    fix(data): add two tv shows objects in tv show data file

--- api/src/data/TvShowData.ts
+++ api/src/data/TvShowData.ts
@@ -197,4 +197,107 @@ export const tvShowData = [
       },
     ],
   },
+
+  {
+    id: "T003",
+    title: "Écho des Étoiles",
+    genre: "talent_show",
+    image: "/api/images/showsTV/echo-etoiles.png",
+    director: "Mathieu Ravel",
+    seasonData: [
+      {
+        seasonYear: 2023,
+        seasonNumber: 2,
+        seasonTVHost: "Clara Montfaucon",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 110,
+          },
+          {
+            numberEpisode: 2,
+            duration: 110,
+          },
+          {
+            numberEpisode: 3,
+            duration: 110,
+          },
+          {
+            numberEpisode: 4,
+            duration: 110,
+          },
+          {
+            numberEpisode: 5,
+            duration: 110,
+          },
+          {
+            numberEpisode: 6,
+            duration: 110,
+          },
+          {
+            numberEpisode: 7,
+            duration: 110,
+          },
+          { numberEpisode: 8, duration: 130 },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: "T004",
+    title: "On Dira Ce Qu'On En Veut",
+    genre: "talk_show",
+    image: "/api/images/showsTV/on-dira-ce-qu-on-en-veut.png",
+    director: "Romain Castelneau",
+    seasonData: [
+      {
+        seasonYear: 2024,
+        seasonNumber: 3,
+        seasonTVHost: "Julien Moreau",
+        episodes: [
+          {
+            numberEpisode: 1,
+            duration: 145,
+          },
+          {
+            numberEpisode: 2,
+            duration: 145,
+          },
+          {
+            numberEpisode: 3,
+            duration: 145,
+          },
+          {
+            numberEpisode: 4,
+            duration: 145,
+          },
+          {
+            numberEpisode: 5,
+            duration: 145,
+          },
+          {
+            numberEpisode: 6,
+            duration: 145,
+          },
+          {
+            numberEpisode: 7,
+            duration: 145,
+          },
+          {
+            numberEpisode: 8,
+            duration: 160,
+          },
+          {
+            numberEpisode: 9,
+            duration: 160,
+          },
+          {
+            numberEpisode: 10,
+            duration: 175,
+          },
+        ],
+      },
+    ],
+  },
 ];
