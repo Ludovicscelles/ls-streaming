8d1d8c0f7c9d57d6cceb4321d323c316ef9a31a8
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-12-19 10:29:37 +0100

    fix(checkClassMethods): correct utils files using to create series and tv-shows, checkClassMethods and initiateData files

--- api/src/utils/old-create-files/createTvShow.ts
+++ api/src/utils/old-create-files/createTvShow.ts
@@ -18,13 +18,13 @@ export function createTvShow(
   // Array of season data, each containing episodes
   // Each season has a year, number, TV host, and list of episodes
   // Each episode has a number and duration
-  seasonData: {
+  seasonTvShowEntities: {
     seasonYear: number;
     seasonNumber: number;
     seasonTVHost: string;
     // Array of episodes in the season
     episodes: {
-      numberEpisode: number;
+      episodeNumber: number;
       duration: number;
     }[];
   }[];
@@ -33,12 +33,12 @@ export function createTvShow(
 }): TvShow {
   // Map through the season data to create SeasonTvShow instances
   // Each SeasonTvShow contains its respective EpisodeTvShow instances
-  const season = data.seasonData.map(
+  const season = data.seasonTvShowEntities.map(
     ({ seasonYear, seasonNumber, seasonTVHost, episodes }) => {
       // Map through the episodes to create EpisodeTvShow instances
       // Each EpisodeTvShow is constructed with its respective properties
       const episodesInstance = episodes.map(
-        (ep) => new EpisodeTvShow(ep.numberEpisode, ep.duration)
+        (ep) => new EpisodeTvShow(ep.episodeNumber, ep.duration)
       );
       // Return a new SeasonTvShow instance with the constructed episodes
       // and other properties from the input data
