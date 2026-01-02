7ee5fe2f8240426749029bf96370af965ea55f71
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-01-02 18:28:02 +0100

    refactor(back-controller): extract getAll for controllers

--- api/src/controllers/controller.ts
+++ api/src/controllers/controller.ts
@@ -1,93 +1,54 @@
 import { Request, Response, RequestHandler } from "express";
 import { MovieLibraryRepository } from "../MovieLibraryRepository";
 import { makeSearchByTitleController } from "./helpers/searchByTitle.helper";
-import { title } from "process";
+import { makeGetAllVideosController } from "./helpers/getAll.helper";
 
 // controller to get all videos
-export function getAllVideosController(
-  // dependency injection of the MovieLibraryRepository
-  // to be able to use its methods
+export const getAllVideosController = (
   movieLibrary: MovieLibraryRepository
-  // return a RequestHandler
-  // which is a function that takes a Request and a Response
-): RequestHandler {
-  // return the RequestHandler
-  // which is an async function that handles the request and response
-  return async (req: Request, res: Response): Promise<Response> => {
-    try {
-      // use the movieLibrary to get all videos
-      const videos = await movieLibrary.getAllVideos();
-      // return the videos as a JSON response
-      return res.json(videos);
-      // handle errors appropriately
-    } catch (error) {
-      // log the error for debugging purposes
-      console.error("Error fetching videos:", error);
-      // return a 500 Internal Server Error response
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
+): RequestHandler => {
+  return makeGetAllVideosController(
+    () => movieLibrary.getAllVideos(),
+    "videos"
+  );
+};
 
 // controller to get all films
-export function getAllFilmsController(
+export const getAllFilmsController = (
   movieLibrary: MovieLibraryRepository
-): RequestHandler {
-  return async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const films = await movieLibrary.getAllFilms();
-      return res.json(films);
-    } catch (error) {
-      console.error("Error fetching films:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
+): RequestHandler => {
+  return makeGetAllVideosController(() => movieLibrary.getAllFilms(), "films");
+};
 
 // controller to get all documentaries
-export function getAllDocumentariesController(
+export const getAllDocumentariesController = (
   movieLibrary: MovieLibraryRepository
-): RequestHandler {
-  return async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const documentaries = await movieLibrary.getAllDocumentaries();
-      return res.json(documentaries);
-    } catch (error) {
-      console.error("Error fetching documentaries:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
+): RequestHandler => {
+  return makeGetAllVideosController(
+    () => movieLibrary.getAllDocumentaries(),
+    "documentaries"
+  );
+};
 
 // controller to get all series
-export function getAllSeriesController(
+export const getAllSeriesController = (
   movieLibrary: MovieLibraryRepository
-): RequestHandler {
-  return async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const series = await movieLibrary.getAllSeries();
-      return res.json(series);
-    } catch (error) {
-      console.error("Error fetching series:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
+): RequestHandler => {
+  return makeGetAllVideosController(
+    () => movieLibrary.getAllSeries(),
+    "series"
+  );
+};
 
 // controller to get all tv shows
-export function getAllTvShowsController(
+export const getAllTvShowsController = (
   movieLibrary: MovieLibraryRepository
-): RequestHandler {
-  return async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const tvShows = await movieLibrary.getAllTvShows();
-      return res.json(tvShows);
-    } catch (error) {
-      console.error("Error fetching tv shows:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-}
+): RequestHandler => {
+  return makeGetAllVideosController(
+    () => movieLibrary.getAllTvShows(),
+    "tv shows"
+  );
+};
 
 // controller to get film by ID
 export function getFilmByIdController(
@@ -333,5 +294,5 @@ export const searchTvShowsController = (
   return makeSearchByTitleController(
     (title: string) => movieLibrary.searchTvShows(title),
     "tv shows"
-  )
-}
+  );
+};
