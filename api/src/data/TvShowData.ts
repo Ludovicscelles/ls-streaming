84be093c6373f67a2b5a038f567cace04acccdd5
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-01-25 18:06:23 +0100

    feat(data): add description to tv shows and their seasons

--- api/src/data/TvShowData.ts
+++ api/src/data/TvShowData.ts
@@ -5,11 +5,15 @@ export const tvShowData = [
     genre: "game_show",
     image: "/api/images/showsTV/traites.png",
     director: "Françis Côté",
+    description:
+      "Dans 'Les Traîtres', un groupe de candidats doit collaborer pour accomplir des missions et accumuler une cagnotte commune, tout en essayant de démasquer les traîtres infiltrés parmi eux. Chaque épisode est rempli de suspense, de stratégies et de retournements de situation, alors que les participants naviguent entre alliances et trahisons pour tenter de remporter le grand prix.",
     seasonTvShowEntities: [
       {
         seasonYear: 2022,
         seasonNumber: 1,
         tvHost: "Éric Antoine",
+        seasonDescription:
+          "La première saison des 'Traîtres' introduit les téléspectateurs à un format captivant où des candidats doivent travailler ensemble pour réussir des missions tout en identifiant les traîtres parmi eux. Animée par Éric Antoine, cette saison met en avant des stratégies complexes, des alliances fragiles et des moments de tension intense, offrant un divertissement palpitant à chaque épisode.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
@@ -45,6 +49,8 @@ export const tvShowData = [
         seasonYear: 2023,
         seasonNumber: 2,
         tvHost: "Éric Antoine",
+        seasonDescription:
+          "La deuxième saison des 'Traîtres' continue de captiver les téléspectateurs avec de nouveaux candidats, des missions encore plus audacieuses et des traîtres plus rusés. Sous la houlette d'Éric Antoine, cette saison promet des rebondissements inattendus, des stratégies élaborées et une intensité dramatique accrue, rendant chaque épisode incontournable.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
@@ -92,6 +98,8 @@ export const tvShowData = [
         seasonYear: 2024,
         seasonNumber: 3,
         tvHost: "Éric Antoine",
+        seasonDescription:
+          "La troisième saison des 'Traîtres' élève encore le niveau de suspense et de stratégie, avec un casting diversifié et des défis innovants. Éric Antoine guide les téléspectateurs à travers une série d'épisodes riches en émotions, où chaque décision peut changer le cours du jeu. Attendez-vous à des alliances surprenantes, des trahisons choquantes et une compétition féroce pour la victoire finale.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
@@ -147,11 +155,15 @@ export const tvShowData = [
     genre: "game_show",
     image: "/api/images/showsTV/fort-boyard.png",
     director: "Françis Côté",
+    description:
+      "Depuis 1990, 'Fort Boyard' est le rendez-vous incontournable des amateurs de défis et d'aventures. Chaque saison, des équipes de candidats relèvent des épreuves physiques et mentales au sein du célèbre fort situé en mer, dans le but de récolter un maximum de clés et de boyards. Animé par Olivier Minne, le jeu combine suspense, esprit d'équipe et divertissement, offrant aux téléspectateurs des moments inoubliables et des performances impressionnantes.",
     seasonTvShowEntities: [
       {
         seasonYear: 2022,
         seasonNumber: 34,
         tvHost: "Olivier Minne",
+        seasonDescription:
+          "La 34ème saison de 'Fort Boyard' continue de captiver les téléspectateurs avec des défis toujours plus audacieux et des équipes de candidats prêtes à tout pour remporter la victoire. Sous la direction d'Olivier Minne, cette saison promet des moments de suspense intenses, des stratégies d'équipe ingénieuses et des performances mémorables au cœur du légendaire fort.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
@@ -204,11 +216,15 @@ export const tvShowData = [
     genre: "talent_show",
     image: "/api/images/showsTV/echo-etoiles.png",
     director: "Mathieu Ravel",
+    description:
+      "Écho des Étoiles est une émission de télévision captivante qui met en lumière les talents émergents de la musique à travers des performances époustouflantes et des jugements d'experts. Chaque saison, des chanteurs, musiciens et groupes de divers horizons se disputent la reconnaissance et la gloire, offrant aux téléspectateurs un spectacle riche en émotions, en surprises et en découvertes artistiques. Animée par une équipe charismatique, l'émission célèbre la passion pour la musique et l'aspiration à réaliser ses rêves.",
     seasonTvShowEntities: [
       {
         seasonYear: 2023,
         seasonNumber: 2,
         tvHost: "Clara Montfaucon",
+        seasonDescription:  
+          "La deuxième saison d'Écho des Étoiles promet d'être encore plus spectaculaire, avec un casting diversifié de talents musicaux prêts à impressionner le public et les juges. Sous la houlette de Clara Montfaucon, cette saison mettra en avant des performances innovantes, des collaborations inattendues et des moments de pure émotion, tout en offrant aux téléspectateurs une expérience musicale inoubliable.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
@@ -250,11 +266,15 @@ export const tvShowData = [
     genre: "talk_show",
     image: "/api/images/showsTV/on-dira-ce-qu-on-en-veut.png",
     director: "Romain Castelneau",
+    description:
+      "'On Dira Ce Qu'On En Veut' est un talk-show dynamique et engageant qui explore les sujets d'actualité, les tendances culturelles et les questions de société à travers des discussions animées et des interviews approfondies. Animé par un présentateur charismatique, l'émission réunit des experts, des célébrités et des invités spéciaux pour offrir aux téléspectateurs une perspective diversifiée et stimulante sur les thèmes qui façonnent notre monde. Avec son format interactif, le show encourage également la participation du public, créant ainsi un espace de dialogue ouvert et enrichissant.",
     seasonTvShowEntities: [
       {
         seasonYear: 2024,
         seasonNumber: 3,
         tvHost: "Julien Moreau",
+        seasonDescription:
+          "La troisième saison d'On Dira Ce Qu'On En Veut continue de captiver les téléspectateurs avec des discussions passionnantes et des invités de renom. Sous la direction de Julien Moreau, cette saison promet d'aborder des sujets encore plus variés et pertinents, tout en offrant un espace pour des débats enrichissants et des perspectives nouvelles. Attendez-vous à des épisodes pleins de surprises, d'analyses approfondies et d'interactions engageantes avec le public.",
         episodeTvShowEntities: [
           {
             episodeNumber: 1,
